c964f2b6413b441645640621acbee0c63033b4dc
RiahFA <134052053+RiahFA@users.noreply.github.com>
2023-05-19 15:59:43 +0000

    Move console script to different directory

--- projects/railsgirls/app/models/idea.rb
+++ projects/railsgirls/app/models/idea.rb
@@ -1,12 +1,2 @@
 class Idea < ApplicationRecord
 end
-
-# This is a little script that changes descriptions of all ideas you have in the database
-# To run it, open a Rails console with `rails console` (from the terminal) and then copy the code below
-def update_description(idea)
-    idea.update!(description: "Moriah changed it")
-end
-
-Idea.all.each do |idea|
-    update_description(idea)
-end
