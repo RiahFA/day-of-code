9f776b33d17aaecb46de3957a4213dd3dd72084b
RiahFA <134052053+RiahFA@users.noreply.github.com>
2023-05-19 15:57:19 +0000

    End of day coaches

--- projects/railsgirls/app/models/idea.rb
+++ projects/railsgirls/app/models/idea.rb
@@ -1,11 +1,12 @@
 class Idea < ApplicationRecord
 end
 
+# This is a little script that changes descriptions of all ideas you have in the database
+# To run it, open a Rails console with `rails console` (from the terminal) and then copy the code below
+def update_description(idea)
+    idea.update!(description: "Moriah changed it")
+end
 
-# def update_description(idea)
-#     idea.update!(description: "Ewa changed it")
-# end
-
-# Idea.all.each do |idea|
-#     update_description(idea)
-# end
+Idea.all.each do |idea|
+    update_description(idea)
+end
--- projects/ruby_scripts/test.rb
+++ projects/ruby_scripts/test.rb
@@ -1,4 +1,7 @@
 
+# In order to run this piece of Ruby code, navigate to projects/ruby_scripts (`cd projects/ruby_scripts`) and then
+# type in the terminal `ruby test.rb`.
+
 name = "Moriah"
 puts "Hello, #{name}"
 
